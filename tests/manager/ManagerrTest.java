8ed121072d61112d09cccaf1a36ec61b2da7e653
rmaliki <rochd.maliki@telecom-bretagne.eu>
2017-05-24 00:41:18 +0800

    adding files

---
+++ tests/manager/ManagerrTest.java
@@ -0,0 +1,27 @@
+package manager;
+
+import org.junit.Test;
+
+import fr.uv1.bettingServices.Manager;
+import fr.uv1.bettingServices.exceptions.AuthenticationException;
+
+
+public class ManagerrTest {
+
+    private static final String MANAGER_PWD = "";
+    private static final String WRONG_PWD = "wrong_pwd";
+    private Manager m=new Manager();
+   
+    
+    
+    @Test
+    public void testAuthenticateMngr() throws AuthenticationException {
+    	
+        m.authenticateMngr(MANAGER_PWD);
+    }
+
+    @Test(expected = AuthenticationException.class)
+    public void testWrongPwd() throws Exception {
+        m.authenticateMngr(WRONG_PWD);
+    }
+}
